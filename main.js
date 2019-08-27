58fd394ea2f967ae11ffe9a0907207d29fcacf66
Elkie Nova <autumn.leaves.on.jupiter@gmail.com>
2019-08-28 02:42:49 +0300

    add menu option, set window title, make save paths main's globals

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  MenuItem,
+  dialog
+} = require('electron');
+
+global.gameHtmlPath = 'test/game.html';
+global.bitsyDataPath = 'test/data.bitsy';
 
 function createWindow () {
-  let win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    fullscreen: true,
+  const { screen } = require('electron');
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({
+    width: width,
+    height: height,
+    fullscreenWindowTitle: true,
     webPreferences: {
       nodeIntegration: true,
       preload: require.resolve('./src/bitsy-savior')
     }
   })
-
   win.loadFile('src/bitsy/editor/index.html');
   win.webContents.openDevTools();
+  win.on('page-title-updated', (e) => e.preventDefault());
+
+  // TODO: make a function to get status string for window title
+  win.setTitle(`${gameHtmlPath}; ${bitsyDataPath}`);
+
+  // TODO: replace with a new menu from template
+  const menu = Menu.getApplicationMenu();
+  const fileMenu = menu.items.find(i => i.label === 'File');
+  const saveCommand = new MenuItem({
+    click: getSavePathFromDialog,
+    type: 'normal',
+    label: 'Save',
+    accelerator: 'CommandOrControl+S'
+  });
+  fileMenu.submenu.append(saveCommand);
+  Menu.setApplicationMenu(menu);
 }
 
+async function getSavePathFromDialog (event, focusedWindow, focusedWebContents) {
+  console.log('clicked on file dialog option');
+  // TODO: attach dialog to window
+  // showSaveDialog returns a promise that resolves into filepath and whether dialog was canceled
+  // dialog title doesn't show up on macos
+  console.log(await dialog.showSaveDialog({ title: 'choose html file where you wish to update game data' }));
+}
+
+// TODO: make a whole new menu from a template to use,
+// instead of electron's default one. do the template in a separate module
+// Menu.setApplicationMenu(thatNewMenu);
 app.on('ready', createWindow);
--- src/bitsy-savior.js
+++ src/bitsy-savior.js
@@ -1,28 +1,28 @@
 const fse = require('fs-extra');
+const {
+  remote
+} = require('electron');
 
-console.log('hiya from bitsy-savior');
 document.addEventListener('DOMContentLoaded', injectBitsySavior);
-
-const gameHtmlPath = 'test/game.html';
-const bitsyDataPath = 'test/data.bitsy';
+console.log('hiya from bitsy-savior');
 
 function injectBitsySavior() {
   console.log('injecting bitsy savior');
   refreshGameDataOrig = window.refreshGameData;
   window.refreshGameData = function () {
     refreshGameDataOrig.call(window);
-    fse.outputFile(bitsyDataPath, getFullGameData())
+    fse.outputFile(remote.getGlobal('bitsyDataPath'), getFullGameData())
       .catch(err => {
         console.error(err);
       });
-    fse.readFile(gameHtmlPath, 'utf8')
+    fse.readFile(remote.getGlobal('gameHtmlPath'), 'utf8')
       .then(bitsyHtml => {
         const updatedGameHtml = bitsyHtml.replace(
           /(<script type="text\/bitsyGameData" id="exportedGameData">)[\s\S]*?(<\/script>)/,
           (m, openingTag, closingTag) => {
             return `${openingTag}\n${getFullGameData()}\n${closingTag}`;
         });
-        return fse.outputFile(gameHtmlPath, updatedGameHtml);
+        return fse.outputFile(remote.getGlobal('gameHtmlPath'), updatedGameHtml);
       })
       .catch(err => {
         console.error(err);
