0c3fc130fdd2278d9a5a9405835e96d2bc7f4824
Elkie Nova <autumn.leaves.on.jupiter@gmail.com>
2020-10-29 22:50:09 +0300

    progress with launcher ui

--- src/launcher/js/launcher.js
+++ src/launcher/js/launcher.js
@@ -3,17 +3,57 @@ const {
     ipcRenderer
 } = require('electron');
 
+let isAddingEditor = false;
+
 let editorListEl;
-let editorInfoEl;
+
+let editorNameEl;
+let editorTypeEl;
+let editorDescriptionEl;
+let editorPathEl;
+
+let editorNameInputEl;
+let editorDescriptionInputEl;
+let editorPathInputEl;
+
+let setUpEditorButtonEl;
+let deleteEditorButtonEl;
+
+let infoDisplayedEls;
+let infoEditableEls;
+
 let storedData;
 
-document.addEventListener('DOMContentLoaded', (event) => {
+function init() {
     editorListEl = document.getElementById('editorList');
-    editorInfoEl = document.getElementById('editorInfo');
+
+    editorNameEl = document.getElementById('editorName');
+    editorTypeEl = document.getElementById('editorType');
+    editorDescriptionEl = document.getElementById('editorDescription');
+    editorPathEl = document.getElementById('editorPath');
+    setUpEditorButtonEl = document.getElementById('setUpEditorButton');
+    deleteEditorButtonEl = document.getElementById('deleteEditorButton');
+
+    editorNameInputEl = document.getElementById('editorNameInput');;
+    editorDescriptionInputEl = document.getElementById('editorDescriptionInput');;
+    editorPathInputEl = document.getElementById('editorPathInput');;
+
+    infoDisplayedEls = Array.prototype.slice.call(document.getElementsByClassName('infoDisplayed'));
+    infoEditableEls = Array.prototype.slice.call(document.getElementsByClassName('infoEditable'));
 
     storedData = remote.getGlobal('storedData');
 
-    // todo: populate bitsy editors list according to data stored in global object
+    updateEditorList();
+    checkSelectedEditor();
+    setInfoEditable(false);
+    updateDisplayedInfo();
+}
+
+function updateEditorList() {
+    // clear the previous list
+    editorListEl.innerHTML = '';
+
+    // populate bitsy editors list according to data stored in global object
     storedData.editors.forEach((editor, index) => {
         const inputEl = document.createElement('input');
         inputEl.type = 'radio';
@@ -30,26 +70,54 @@ document.addEventListener('DOMContentLoaded', (event) => {
         editorListEl.appendChild(labelEl);
         editorListEl.appendChild(document.createElement('br'));
     });
+}
 
+function checkSelectedEditor() {
     const editorListControls = editorListEl.elements;
     for (let index = 0; index < editorListControls.length; index++) {
         if (index === storedData.editorIndex) {
             editorListControls[index].checked = true;
         }
     }
-
-    editorInfoEl.innerText = storedData.editors[storedData.editorIndex].description;
-});
+}
 
 function onAddEditorShow(event) {
+    isAddingEditor = true;
+
+    // todo: uncheck all editors in the list, but don't change editorIndex in data yet
+    const editorListControls = editorListEl.elements;
+    for (let index = 0; index < editorListControls.length; index++) {
+        editorListControls[index].checked = false;
+    }
+
+    // todo: reset editable fields in editor info panel
+    clearInputs();
+
+    // for now it can only be local
+    editorTypeEl.innerText = 'local';
+
+    // show editable fields in editor info panel
+    setInfoEditable(true);
+}
+
+function onConfirmEditor(event) {
+    // todo
+    isAddingEditor = false;
+    setInfoEditable(false);
+    checkSelectedEditor();
+}
+
+function onCancelEditor(event) {
+    isAddingEditor = false;
+    setInfoEditable(false);
+    checkSelectedEditor();
+}
+
+function onSetUpEditor(event) {
     // todo
 }
 
-function onAddEditorConfirm(event) {
-    // todo
-}
-
-function onRemoveEditor(event) {
+function onDeleteEditor(event) {
     // todo
 }
 
@@ -61,10 +129,56 @@ function onSwitchEditor(event) {
     ipcRenderer.send('saveData');
 
     // update ui
-    editorInfoEl.innerText = storedData.editors[storedData.editorIndex].description;
+    updateDisplayedInfo();
+
+    if (isAddingEditor) onCancelEditor(event);
+}
+
+function updateDisplayedInfo() {
+    const curEditor =  storedData.editors[storedData.editorIndex];
+    editorNameEl.innerText = curEditor.name;
+    editorTypeEl.innerText = curEditor.type.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1").toLowerCase();
+    editorDescriptionEl.innerText = curEditor.description;
+    if (curEditor.type === 'builtinVanilla') {
+        editorPathEl.style.display = "none";
+        setUpEditorButtonEl.style.display = "none";
+        deleteEditorButtonEl.style.display = "none";
+    } else {
+        editorPathEl.style.display = "block";
+        setUpEditorButtonEl.style.display = "inline";
+        deleteEditorButtonEl.style.display = "inline";
+        editorPathEl.innerText = curEditor.editorPath;
+    }
+}
+
+function updateInputs() {
+    const curEditor =  storedData.editors[storedData.editorIndex];
+    
+    editorNameInputEl.value = curEditor.name;
+    editorDescriptionInputEl.value = curEditor.description;
+    editorPathInputEl.value = curEditor.editorPath;
+}
+
+function clearInputs() {
+    editorNameInputEl.value = '';
+    editorDescriptionInputEl.value = '';
+    editorPathInputEl.value = '';
+}
+
+function setInfoEditable(bool) {
+    infoEditableEls.forEach(el => {
+        const on = el.tagName === 'SPAN'? 'inline': 'block';
+        el.style.display = bool? on: 'none';
+    });
+    infoDisplayedEls.forEach(el => {
+        const on = el.tagName === 'SPAN'? 'inline': 'block';
+        el.style.display = bool? 'none': on;
+    });
 }
 
 function onRunEditor(event) {
     console.log(`i'm going to run editor number ${storedData.editorIndex}, ${storedData.editors[storedData.editorIndex].name}!`)
     ipcRenderer.send('runEditor');
 }
+
+document.addEventListener('DOMContentLoaded', init);
